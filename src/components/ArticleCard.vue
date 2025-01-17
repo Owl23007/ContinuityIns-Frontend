779ccb5732392c63006cbc2b7db9fe5028592aec
Oii Woof <w15072362290@163.com>
2025-01-17 21:08:48 +0800

    重构样式，移除 ArticleCard.css，调整 App.css 和 Home.vue 中的布局和样式

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,50 +1,207 @@
 <template>
-    <div class="article-card">
-      <div class="info">
-        <div class="coverimg">
-          <img :src= "coverImage" alt="Article Image" />
+  <div class="article-card">
+    <a :href="titleLink" class="coverimg">
+      <img :src="coverImage"  alt="Article Image" />
+      <div class="icons">
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+            <path d="M12 4.99805C9.48178 4.99805 7.283 5.12616 5.73089 5.25202C4.65221 5.33949 3.81611 6.16352 3.72 7.23254C3.60607 8.4998 3.5 10.171 3.5 11.998C3.5 13.8251 3.60607 15.4963 3.72 16.76355C3.81611 17.83255 4.65221 18.6566 5.73089 18.7441C7.283 18.8699 9.48178 18.998 12 18.998C14.5185 18.998 16.7174 18.8699 18.2696 18.74405C19.3481 18.65655 20.184 17.8328 20.2801 16.76405C20.394 15.4973 20.5 13.82645 20.5 11.998C20.5 10.16965 20.394 8.49877 20.2801 7.23205C20.184 6.1633 19.3481 5.33952 18.2696 5.25205C16.7174 5.12618 14.5185 4.99805 12 4.99805zM5.60965 3.75693C7.19232 3.62859 9.43258 3.49805 12 3.49805C14.5677 3.49805 16.8081 3.62861 18.3908 3.75696C20.1881 3.90272 21.6118 5.29278 21.7741 7.09773C21.8909 8.3969 22 10.11405 22 11.998C22 13.88205 21.8909 15.5992 21.7741 16.8984C21.6118 18.7033 20.1881 20.09335 18.3908 20.23915C16.8081 20.3675 14.5677 20.498 12 20.498C9.43258 20.498 7.19232 20.3675 5.60965 20.2392C3.81206 20.0934 2.38831 18.70295 2.22603 16.8979C2.10918 15.5982 2 13.8808 2 11.998C2 10.1153 2.10918 8.39787 2.22603 7.09823C2.38831 5.29312 3.81206 3.90269 5.60965 3.75693z" fill="currentColor"></path>
+            <path d="M14.7138 10.96875C15.50765 11.4271 15.50765 12.573 14.71375 13.0313L11.5362 14.8659C10.74235 15.3242 9.75 14.7513 9.75001 13.8346L9.75001 10.1655C9.75001 9.24881 10.74235 8.67587 11.5362 9.13422L14.7138 10.96875z" fill="currentColor"></path>
+          </svg>
+          {{ views }}
+        </span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+            <path d="M12 4.99805C9.48178 4.99805 7.283 5.12616 5.73089 5.25202C4.65221 5.33949 3.81611 6.16352 3.72 7.23254C3.60607 8.4998 3.5 10.171 3.5 11.998C3.5 13.8251 3.60607 15.4963 3.72 16.76355C3.81611 17.83255 4.65221 18.6566 5.73089 18.7441C7.283 18.8699 9.48178 18.998 12 18.998C14.5185 18.998 16.7174 18.8699 18.2696 18.74405C19.3481 18.65655 20.184 17.8328 20.2801 16.76405C20.394 15.4973 20.5 13.82645 20.5 11.998C20.5 10.16965 20.394 8.49877 20.2801 7.23205C20.184 6.1633 19.3481 5.33952 18.2696 5.25205C16.7174 5.12618 14.5185 4.99805 12 4.99805zM5.60965 3.75693C7.19232 3.62859 9.43258 3.49805 12 3.49805C14.5677 3.49805 16.8081 3.62861 18.3908 3.75696C20.1881 3.90272 21.6118 5.29278 21.7741 7.09773C21.8909 8.3969 22 10.11405 22 11.998C22 13.88205 21.8909 15.5992 21.7741 16.8984C21.6118 18.7033 20.1881 20.09335 18.3908 20.23915C16.8081 20.3675 14.5677 20.498 12 20.498C9.43258 20.498 7.19232 20.3675 5.60965 20.2392C3.81206 20.0934 2.38831 18.70295 2.22603 16.8979C2.10918 15.5982 2 13.8808 2 11.998C2 10.1153 2.10918 8.39787 2.22603 7.09823C2.38831 5.29312 3.81206 3.90269 5.60965 3.75693z" fill="currentColor"></path>
+            <path d="M15.875 10.75L9.875 10.75C9.46079 10.75 9.125 10.4142 9.125 10C9.125 9.58579 9.46079 9.25 9.875 9.25L15.875 9.25C16.2892 9.25 16.625 9.58579 16.625 10C16.625 10.4142 16.2892 10.75 15.875 10.75z" fill="currentColor"></path>
+            <path d="M17.375 14.75L11.375 14.75C10.9608 14.75 10.625 14.4142 10.625 14C10.625 13.5858 10.9608 13.25 11.375 13.25L17.375 13.25C17.7892 13.25 18.125 13.5858 18.125 14C18.125 14.4142 17.7892 14.75 17.375 14.75z" fill="currentColor"></path>
+            <path d="M7.875 10C7.875 10.4142 7.53921 10.75 7.125 10.75L6.625 10.75C6.21079 10.75 5.875 10.4142 5.875 10C5.875 9.58579 6.21079 9.25 6.625 9.25L7.125 9.25C7.53921 9.25 7.875 9.58579 7.875 10z" fill="currentColor"></path>
+            <path d="M9.375 14C9.375 14.4142 9.03921 14.75 8.625 14.75L8.125 14.75C7.71079 14.75 7.375 14.4142 7.375 14C7.375 13.5858 7.71079 13.25 8.125 13.25L8.625 13.25C9.03921 13.25 9.375 13.5858 9.375 14z" fill="currentColor"></path>
+          </svg>
+          {{ favorites }}
+        </span>
+        <span class="video-duration">{{ duration }}</span>
+      </div>
+    </a>
+    <div class="details">
+      <div class="article-title">
+        <a :href="titleLink">{{ title }}</a> <!-- 标题链接 -->
+      </div>
+      <div class="article-info">
+        <div class="article-author">
+          <a :href="authorLink">{{ author }}</a> <!-- 作者链接 -->
         </div>
-        <div class="details">
-          <div class="article-title">{{ title }}</div>
-          <div class="article-author">作者: {{ author }}</div>
-          <div class="article-time">发布时间: {{ time }}</div>
-          <div class="article-views">浏览量: {{ views }}</div>
-          <div class="article-favorites">收藏: {{ favorites }}</div>
-        </div>
+        <div class="article-time">{{ time }}</div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ArticleCard',
-    props: {
-      coverImage: {
-        type: String,
-        required: true
-      },
-      title: {
-        type: String,
-        required: true
-      },
-      author: {
-        type: String,
-        required: true
-      },
-      time: {
-        type: String,
-        required: true
-      },
-      views: {
-        type: Number,
-        required: true
-      },
-      favorites: {
-        type: Number,
-        required: true
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleCard',
+  props: {
+    coverImage: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    time: {
+      type: String,
+      required: true
+    },
+    views: {
+      type: Number,
+      required: true
+    },
+    favorites: {
+      type: Number,
+      required: true
+    },
+    duration: {
+      type: String,
+      required: true
+    },
+    titleLink: { // 新增标题链接属性
+      type: String,
+      required: true
+    },
+    authorLink: { // 新增作者链接属性
+      type: String,
+      required: true
     }
-  };
-  </script>
-  
-  <style scoped src = "@/assets/css/ArticleCard.css"></style>
+  }
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+.article-card {
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 16px 0;
+  width: 100%;
+  max-width: 400px; /* 调整卡片的最大宽度 */
+  max-width: 300px; /* 调整卡片的最大宽度 */
+  background-color: #ffffff00;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s, box-shadow 0.2s;
+}
+
+.article-card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.coverimg {
+  position: relative;
+  text-decoration: none;
+  display: block;
+}
+
+.coverimg img {
+  width: 100%;
+  border-radius: 8px;
+  height: auto;
+  aspect-ratio: 16 / 9;
+}
+
+.icons {
+  position: absolute;
+  bottom: 4px; /* 上移阴影位置 */
+  left: 0; /* 确保左边对齐 */
+  right: 0; /* 确保右边对齐 */
+  width: calc(100% - 8px); /* 确保宽度不超过 */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px 4px;
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)); /* 添加渐变灰背景 */
+  border-bottom-left-radius: 8px; /* 添加下圆角 */
+  border-bottom-right-radius: 8px; /* 添加下圆角 */
+}
+
+.icons span {
+  display: flex;
+  align-items: center;
+  color: #fff;
+  margin-right: 3px;
+}
+
+.icons svg {
+  margin-left: 3px;
+  margin-right: 3px;
+}
+
+.video-duration {
+  margin-left: auto;
+  padding-right: 0px;
+  color: #fff;
+  margin-right: 0px; 
+}
+
+.details {
+  padding: 16px;
+  text-decoration: none;
+  color: inherit;
+  display: block;
+  background-color: #fff; /* 添加白色背景 */
+  border-radius: 8px; /* 添加圆角 */
+  margin-top: 1-2px; /* 下移1-2px */
+}
+
+.article-title {
+  font-size: 1.25em;
+  font-weight: 500; /* 调整字体粗细 */
+  margin-bottom: 8px;
+  font-family: 'Roboto', sans-serif; /* 设置大卡片标题的字体 */
+  transition: color 0.2s; /* 添加颜色过渡效果 */
+  user-select: none; /* 禁用文本选择 */
+}
+
+.article-title a {
+  color: inherit;
+  text-decoration: none;
+  transition: color 0.2s;
+  cursor: pointer; /* 鼠标变成手指 */
+}
+
+.article-title a:hover {
+  color: rgb(29, 104, 195); /* 鼠标悬停时变蓝 */
+}
+
+.article-info {
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.875em;
+  color: #586069;
+  user-select: none; /* 禁用文本选择 */
+}
+
+.article-author,
+.article-time {
+  margin-bottom: 4px;
+  user-select: none; /* 禁用文本选择 */
+}
+
+.article-author a {
+  color: inherit;
+  text-decoration: none;
+  transition: color 0.2s;
+  cursor: pointer; /* 鼠标变成手指 */
+}
+
+.article-author a:hover {
+  color: rgb(29, 104, 195); /* 鼠标悬停时变蓝 */
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,16 +7,30 @@
     <main class="center-container">
       <h1>今日推荐</h1>
       <div class="cards-container">
-        <div class="large-card">
-          <ArticleCard
-            coverImage="https://tse2-mm.cn.bing.net/th/id/OIP-C._0gTCowUaUF9RdEeDEoeKgHaE7?w=264&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
-            title="大卡片标题" author="大卡片作者" time="2023-10-01" views="1234" favorites="567" />
-        </div>
-        <div class="small-cards">
-          <ArticleCard v-for="i in 4" :key="i"
-            coverImage="https://tse2-mm.cn.bing.net/th/id/OIP-C._0gTCowUaUF9RdEeDEoeKgHaE7?w=264&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
-            title="小卡片标题" author="小卡片作者" time="2023-10-01" views="1234" favorites="567" />
-        </div>
+        <ArticleCard
+          class="large-card"
+          coverImage="https://tse2-mm.cn.bing.net/th/id/OIP-C._0gTCowUaUF9RdEeDEoeKgHaE7?w=264&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+          title="大卡片标题"
+          author="大卡片作者"
+          time="4小时前"
+          views="1234"
+          favorites="567"
+          duration="10:00"
+          titleLink="https://www.baidu.com"
+          authorLink="https://www.163.com"
+        />
+        <ArticleCard
+          v-for="i in 4"
+          :key="i"
+          class="small-card"
+          coverImage="https://tse2-mm.cn.bing.net/th/id/OIP-C._0gTCowUaUF9RdEeDEoeKgHaE7?w=264&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+          title="小卡片标题"
+          author="小卡片作者"
+          time="10-01"
+          views="1234"
+          favorites="567"
+          duration="03:00"
+        />
       </div>
     </main>
   </div>
@@ -40,67 +54,27 @@ body {
   font-family: 'Roboto', sans-serif;
 }
 
-.container {
-    background-color: #ffffff;
-    border: 1px solid #e1e4e8;
-    border-radius: 6px;
-    box-shadow: 0 1px 3px rgba(27, 31, 35, 0.12), 0 8px 24px rgba(27, 31, 35, 0.12);
-    padding: 20px;
-    width: 80%;
-    max-width: 1200px;
-    margin-bottom: 20px;
-    /* 这是你选中的样式 */
-}
-
 .main-container {
-    display: flex;
-    margin-top: 100px;
+  background-color: #f0f0f0;
+  display: flex;
+  margin-top: 100px;
 }
 
 .sidebar {
-    width: 10%;
-    padding: 20px;
-    background-color: #f4f4f4;
+  width: 10%;
+  padding: 20px;
+  background-color: #f0f0f0;
 }
 
 .center-container {
-    display: flex;
-    margin-top: 80px;
-    width: 70%;
-    height: 450px;
-    border: 1px solid #e1e4e8;
-    border-radius: 6px;
-    box-shadow: 0 1px 3px rgba(27, 31, 35, 0.12), 0 8px 24px rgba(27, 31, 35, 0.12);
-    background-color: #ffffff;
-    padding: 16px;
+  flex: 1;
+  padding: 20px;
 }
 
 .cards-container {
-    display: flex;
-    width: 100%;
-    height: 100%; 
-}
-
-.large-card {
-    flex: 1 1 100%;
-    height: 100%;
-    width: 40%;
-    margin-top: 0px;
-}
-
-.small-cards {
-    display: flex;
-    flex-wrap: wrap;
-    height: 100%;
-    width: 100%;
-    gap: 20px;
-    display: flex;
-    justify-content: space-between;
-}
-
-.small-cards>* {
-    flex: 1 1 calc(50% - 20px);
-    height: calc(50% - 10px);
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
 }
 
 </style>
